d5a100a22fbd4161b2a6f600e4b64103aa63e4d0
Vili Ståhlberg <vili@sivulabra.fi>
2024-02-02 15:56:25 +0200

    Update chart coloring and style, extract /salkku data section to its own component

--- src/routes/salkku/+page.svelte
+++ src/routes/salkku/+page.svelte
@@ -7,57 +7,7 @@
 		CardContent
 	} from '$lib/components/ui/card';
 	import { Icon } from '$lib/components/ui/icon';
-	import type { Holding } from '$lib/types';
-	import Chart from './Chart.svelte';
-
-	let selectedIndex: number | null = null;
-
-	const data: Holding[] = [
-		{
-			title: 'Admicom Oyj',
-			weight: 41.5,
-			acquisitionPrice: 41.19,
-			profit: -0.57,
-			color: '#cce04c'
-		},
-		{
-			title: 'Kesko Corporation B',
-			weight: 24.4,
-			acquisitionPrice: 18.38,
-			profit: -6.74,
-			color: '#ed722e'
-		},
-		{
-			title: 'Nordea Bank Abp',
-			weight: 11.9,
-			acquisitionPrice: 10.03,
-			profit: 4.56,
-			color: '#0e7490'
-		},
-		{
-			title: 'Tokmanni Group Oyj',
-			weight: 10.7,
-			acquisitionPrice: 12.24,
-			profit: 3.03,
-			color: '#cd3c2f'
-		},
-		{ title: 'Olvi Plc A', weight: 4.9, acquisitionPrice: 28.02, profit: 5.44, color: '#eaeaea' },
-		{ title: 'Gofore Plc', weight: 4.3, acquisitionPrice: 25.91, profit: -6.61, color: '#fb923c' },
-		{
-			title: 'Mandatum Oyj',
-			weight: 0.3,
-			acquisitionPrice: 0,
-			profit: 0,
-			color: '#262626'
-		},
-		{
-			title: 'Käteinen',
-			weight: 2.0,
-			acquisitionPrice: 0,
-			profit: 0,
-			color: '#22c55e'
-		}
-	];
+	import DataSection from './data-section.svelte';
 </script>
 
 <svelte:head>
@@ -117,41 +67,7 @@
 		</Card>
 	</div>
 </div>
-<div class="container container-visualization">
-	<div class="cols cols-data">
-		<div class="col col-table">
-			<table>
-				<thead>
-					<tr>
-						<th>Nimi</th>
-						<th>Keskikurssi</th>
-						<th>Tuotto, %</th>
-						<th>Painoarvo, %</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each data as row, index}
-						<tr
-							on:mouseover={() => (selectedIndex = index)}
-							on:focus={() => (selectedIndex = index)}
-							on:mouseleave={() => (selectedIndex = null)}
-						>
-							<td>{row.title}</td>
-							<td>{row.acquisitionPrice.toFixed(2).replace('.', ',')} EUR</td>
-							<td class="profit" class:positive={row.profit >= 0} class:negative={row.profit < 0}
-								>{row.profit.toFixed(1).replace('.', ',')} %</td
-							>
-							<td>{row.weight.toFixed(1).replace('.', ',')} %</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
-		<div class="col col-chart">
-			<Chart {data} {selectedIndex} />
-		</div>
-	</div>
-</div>
+<DataSection />
 
 <style lang="scss">
 	.container-banner {
@@ -161,15 +77,6 @@
 	.container-summary {
 		padding-top: 1rem;
 		padding-bottom: 1rem;
-	}
-
-	.container-visualization {
-		padding-top: 0;
-
-		@media (max-width: calc(1000px - 1px)) {
-			padding-left: 0;
-			padding-right: 0;
-		}
 	}
 
 	.description {
@@ -207,67 +114,6 @@
 
 			:global(svg) {
 				color: var(--color-primary);
-			}
-		}
-	}
-
-	.cols-data {
-		display: grid;
-		row-gap: 3rem;
-
-		@media (min-width: 1000px) {
-			grid-template-columns: 3fr 2fr;
-			column-gap: 3rem;
-		}
-	}
-
-	.col-table {
-		border: 1px solid var(--color-border);
-		border-radius: 6px;
-		overflow-x: auto;
-	}
-
-	.col-chart {
-		align-self: center;
-	}
-
-	table {
-		text-indent: 0;
-		border-color: inherit;
-		border-collapse: collapse;
-		text-align: left;
-		color: #f9fafb;
-		font-size: 14px;
-		width: 100%;
-
-		@media (min-width: 600px) {
-			font-size: 18px;
-		}
-
-		th,
-		td {
-			font-size: 16px;
-			padding: 8px 14px;
-			white-space: nowrap;
-
-			@media (min-width: 600px) {
-				padding: 16px 24px;
-			}
-		}
-
-		th {
-			color: #a1a1aa;
-			font-weight: var(--font-weight-medium);
-		}
-
-		tr {
-			border-bottom: 1px solid var(--color-border);
-			transition-property: background-color;
-			transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-			transition-duration: 0.15s;
-
-			&:hover {
-				background-color: hsl(215 27.9% 16.9%/0.5);
 			}
 		}
 	}
---
+++ src/routes/salkku/data-section.svelte
@@ -0,0 +1,163 @@
+<script lang="ts">
+	import type { Holding } from '$lib/types';
+	import Chart from './chart.svelte';
+
+	let selectedIndex: number | undefined;
+
+	const data: Holding[] = [
+		{
+			title: 'Admicom Oyj',
+			weight: 41.5,
+			acquisitionPrice: 41.19,
+			profit: -0.57,
+			color: '#cce04c'
+		},
+		{
+			title: 'Kesko Corporation B',
+			weight: 24.4,
+			acquisitionPrice: 18.38,
+			profit: -6.74,
+			color: '#ed722e'
+		},
+		{
+			title: 'Nordea Bank Abp',
+			weight: 11.9,
+			acquisitionPrice: 10.03,
+			profit: 4.56,
+			color: '#0e7490'
+		},
+		{
+			title: 'Tokmanni Group Oyj',
+			weight: 10.7,
+			acquisitionPrice: 12.24,
+			profit: 3.03,
+			color: '#cd3c2f'
+		},
+		{ title: 'Olvi Plc A', weight: 4.9, acquisitionPrice: 28.02, profit: 5.44, color: '#eaeaea' },
+		{ title: 'Gofore Plc', weight: 4.3, acquisitionPrice: 25.91, profit: -6.61, color: '#fb923c' },
+		{
+			title: 'Mandatum Oyj',
+			weight: 0.3,
+			acquisitionPrice: 0,
+			profit: 0,
+			color: '#262626'
+		},
+		{
+			title: 'Käteinen',
+			weight: 2.0,
+			acquisitionPrice: 0,
+			profit: 0,
+			color: '#22c55e'
+		}
+	];
+</script>
+
+<div class="container container-visualization">
+	<div class="cols cols-data">
+		<div class="col col-table">
+			<table>
+				<thead>
+					<tr>
+						<th>Nimi</th>
+						<th>Keskikurssi</th>
+						<th>Tuotto, %</th>
+						<th>Painoarvo, %</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each data as row, index}
+						<tr
+							on:mouseover={() => (selectedIndex = index)}
+							on:focus={() => (selectedIndex = index)}
+							on:mouseleave={() => (selectedIndex = undefined)}
+						>
+							<td>{row.title}</td>
+							<td>{row.acquisitionPrice.toFixed(2).replace('.', ',')} EUR</td>
+							<td class="profit" class:positive={row.profit >= 0} class:negative={row.profit < 0}>
+								{row.profit.toFixed(1).replace('.', ',')} %
+							</td>
+							<td>{row.weight.toFixed(1).replace('.', ',')} %</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+		<div class="col col-chart">
+			<Chart {data} {selectedIndex} />
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.container-visualization {
+		padding-top: 0;
+
+		@media (max-width: calc(1000px - 1px)) {
+			padding-left: 0;
+			padding-right: 0;
+		}
+	}
+
+	.cols-data {
+		display: grid;
+		row-gap: 3rem;
+
+		@media (min-width: 1000px) {
+			grid-template-columns: 4fr 3fr;
+			column-gap: 2.2rem;
+		}
+	}
+
+	.col-table {
+		border: 1px solid var(--color-border);
+		border-radius: 6px;
+		overflow-x: auto;
+	}
+
+	.col-chart {
+		align-self: center;
+		// padding-left: var(--padding-container-horizontal);
+		// padding-right: var(--padding-container-horizontal);
+	}
+
+	table {
+		text-indent: 0;
+		border-color: inherit;
+		border-collapse: collapse;
+		text-align: left;
+		color: #f9fafb;
+		font-size: 14px;
+		width: 100%;
+
+		@media (min-width: 600px) {
+			font-size: 18px;
+		}
+
+		th,
+		td {
+			font-size: 16px;
+			padding: 8px 14px;
+			white-space: nowrap;
+
+			@media (min-width: 600px) {
+				padding: 16px 24px;
+			}
+		}
+
+		th {
+			color: #a1a1aa;
+			font-weight: var(--font-weight-medium);
+		}
+
+		tr {
+			border-bottom: 1px solid var(--color-border);
+			transition-property: background-color;
+			transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+			transition-duration: 0.15s;
+
+			&:hover {
+				background-color: hsl(215 27.9% 16.9%/0.5);
+			}
+		}
+	}
+</style>
